7bb581af0d8e8625e261ec4686733d201d18e121
at-shota55 <shota@DESKTOP-GO4MDN8.localdomain>
2021-06-06 04:34:09 +0900

    re

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
-// console.log('Hello, Webpack + TypeScript!')
-// const log = (message: string): void => {
-//     console.log(message)
-// }
-
-// log('Hello, Webpack + TypeScript!')
-
 import * as React from 'react'
 import { render } from 'react-dom'
 
-const Main = (<h1>Markdown Editor</h1>)
+const Main = (<h1>Markdown Editor Internet</h1>)
 
 render(Main, document.getElementById('app'))
